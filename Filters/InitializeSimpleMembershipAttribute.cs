13915717e12484a12429702797d7f7df78d6f6cd
Кирилл Гарькин <Little_idler@mail.ru>
2016-12-09 22:57:51 +0300

    init

---
+++ Filters/InitializeSimpleMembershipAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+using EDMS.Models;
+
+namespace EDMS.Filters {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class InitializeSimpleMembershipAttribute : ActionFilterAttribute {
+        private static SimpleMembershipInitializer _initializer;
+        private static object _initializerLock = new object();
+        private static bool _isInitialized;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext) {
+            LazyInitializer.EnsureInitialized(ref _initializer, ref _isInitialized, ref _initializerLock);
+        }
+
+        private class SimpleMembershipInitializer {
+            public SimpleMembershipInitializer() {
+                Database.SetInitializer<UsersContext>(null);
+
+                try {
+                    using (var context = new UsersContext()) {
+                        if (!context.Database.Exists()) {
+                            ((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
+                        }
+                    }
+
+                    WebSecurity.InitializeDatabaseConnection("db", "UserProfile", "ID", "LOGIN", autoCreateTables: false);
+                } catch (Exception ex) {
+                    throw new InvalidOperationException("Не удалось инициализировать базу данных ASP.NET Simple Membership", ex);
+                }
+            }
+        }
+    }
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EDMS.Models {
+    public class UserRole {
+        private String code;
+        private String name;
+
+        public String Name { get { return name; } }
+        public String Code { get { return code; } }
+
+        public static readonly UserRole ADMINISTRATOR = new UserRole("ADMINISTRATOR", "Администратор");
+        public static readonly UserRole MODERATOR = new UserRole("MODERATOR", "Модератор");
+        public static readonly UserRole CLIENT = new UserRole("CLIENT", "Клиент");
+
+        private UserRole(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+    }
+}
